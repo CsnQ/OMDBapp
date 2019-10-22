b749d8552995b9235235abb56996015658334eb0
Catherine Quilley <catherine.quilley@nology.io>
2019-10-22 17:52:03 +0100

    added in search bar and function to take text input. need to add in a stopper to prevent page refresh

--- src/js/main.ts
+++ src/js/main.ts
@@ -3,6 +3,16 @@ import { helloWorld,buildList, buildCard} from './renderHTML/render-html';
 const searchTerm = "lord"
 const url = `http://www.omdbapi.com/?s=${searchTerm}&apikey=831d3029`
 
+const movieSearch = document.getElementById("search-bar");
+movieSearch.addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+     let userSearchField = document.getElementById("search-bar") as HTMLInputElement; 
+     let userSearchValue = userSearchField.value; 
+       console.log ("nice button pressing")
+       console.log(userSearchValue);
+    }
+});
+
 fetch(url)
 //fetches the data 
 .then((response)=> response.json())
