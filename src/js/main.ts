d85b2139c955c9b696922333a7bb968a9097ec72
Catherine Quilley <catherine.quilley@nology.io>
2019-10-22 18:21:09 +0100

    added search that does not refresh the page

--- src/js/main.ts
+++ src/js/main.ts
@@ -4,8 +4,11 @@ const searchTerm = "lord"
 const url = `http://www.omdbapi.com/?s=${searchTerm}&apikey=831d3029`
 
 const movieSearch = document.getElementById("search-bar");
-movieSearch.addEventListener("keyup", function(event) {
+
+movieSearch.addEventListener("keydown", function(event) {
+
     if (event.key === "Enter") {
+    event.preventDefault();
      let userSearchField = document.getElementById("search-bar") as HTMLInputElement; 
      let userSearchValue = userSearchField.value; 
        console.log ("nice button pressing")
